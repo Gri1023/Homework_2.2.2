715bf76ff850ed89f73843a102783da83a8db8e1
Gri1023 <hryhorii.bavykin@gmail.com>
2022-10-11 20:06:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String students[] = {"Alissa","Ben","Charlie","Dianna"};
+        String grades[] = {"B","D","B","A"};
+
+        for (int i=0;i<students.length;i++) {
+            System.out.println(students[i] + " has " + grades[i]);
+        }
+    }
+}
